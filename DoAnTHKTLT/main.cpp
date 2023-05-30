27a4946201c6062dd7d36e791b65677a7c83a8a5
TuNgoc233 <128065959+TuNgoc233@users.noreply.github.com>
2023-05-30 10:27:52 +0700

    insert,save

--- DoAnTHKTLT/main.cpp
+++ DoAnTHKTLT/main.cpp
@@ -32,6 +32,36 @@ int main() {
 			RemoveDuplicates(phead);
 			Output_List(phead);
 		}
+				while (cmd == "insert") {
+			cout << "command is *insert position value*\n";
+			cout << "Command > ";
+			getline(cin, cmd);
+		}
+
+		if (cmd.substr(0, 6) == "insert") {
+			int dodai1 = 0;
+			for (int i = 7; i <cmd.length(); i++) {
+				if (cmd[i] == ' ') {
+					dodai1 = i - 7;
+					break;
+				}
+			}
+			string s1 = cmd.substr(7, dodai1);
+			int x = stoi(s1);
+			int dodai2 = 0;
+			dodai2 = cmd.size() - 9;
+			string s2 = cmd.substr(9, dodai2);
+			int y = stoi(s2);
+			insertAt(phead, ptail, y, x-1);
+			Output_List(phead);
+		}
+		if (cmd == "save") {
+			Save_List(phead);
+			cout << "Numbers have been stored." << endl;
+		}
+		if (cmd == "quit") {
+			break;
+		}
 
 		/*int chose;
 		cin >> chose;
